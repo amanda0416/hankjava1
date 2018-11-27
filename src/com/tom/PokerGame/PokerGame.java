7005513d0507deeefa0af0774cc9bf52a6e6ff7e
Amanda <amanda212.chen@gmail.com>
2018-11-27 12:02:37 +0800

    11/27

--- src/com/tom/PokerGame/PokerGame.java
+++ src/com/tom/PokerGame/PokerGame.java
@@ -5,21 +5,59 @@ import java.util.Random;
 public class PokerGame {
 
 	public static void main(String[] args) {
-		Random random = new Random();
+		/*int[]nums = new int[5];
+		nums[0] = 77;
+		nums[4] = 98;
+		System.out.println(nums[4]);
+		System.out.println(nums.length);
+		for(int i = 0;i<nums.length;i++){
+			System.out.println(nums[i]);
+		}*/
+		
+		char a = 0x2660;
+		char b = 0x2665;
+		char c = 0x2666;
+		char d = 0x2663;
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+		int[]poker = {1,2,3,4,5,6,7,8,9,10,11,12,13};
+		System.out.println(poker);
+		
+		/*Random random = new Random();
 		int pokerN = random.nextInt(13)+1;
 		System.out.print(pokerN);
-		int pokerF = random.nextInt(4)+1;
-		char grading ='S';
-		if (pokerF==1 ){
-			grading = 'H';
-		}else if(pokerF==2 ){
-			grading = 'C';
-		}else if(pokerF==3 ){
-			grading = 'S';
-		}else if(pokerF==4 ){
-			grading = 'D';
+		int pokerF = random.nextInt(4);
+		char f =0;
+		switch(pokerF){
+		case 0:
+			f ='S';
+			break;
+		case 1:
+			f ='H';
+			break;
+		case 2:
+			f ='D';
+			break;
+		case 3:
+			f ='C';
+			break;
 		}
-		System.out.print(grading);
+		System.out.print(f);*/
+		
+		/*char p =0;
+		if (pokerF==0 ){
+			p = 'S';
+		}else if(pokerF==1 ){
+			p = 'H';
+		}else if(pokerF==2 ){
+			p = 'D';
+		}else if(pokerF==3 ){
+			p = 'C';
+		}
+		System.out.print(p);*/
 	}
 
 }
